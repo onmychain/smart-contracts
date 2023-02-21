a71046a4421ed15a164d6aff7d66585036062565
Cyrille Derche <c.derche@me.com>
2023-02-21 16:11:53 +0100

    startTime and duration

--- contracts/LinearVesting.sol
+++ contracts/LinearVesting.sol
@@ -7,11 +7,17 @@ contract LinearVesting {
     using SafeERC20 for IERC20;
 
     IERC20 public token;
+    
+    uint public startTime;
+    uint public duration;
 
     mapping(address => uint) public allocation;
 
-    constructor(IERC20 token_, address[] memory recipients_, uint[] memory allocations_) {
+
+    constructor(IERC20 token_, address[] memory recipients_, uint[] memory allocations_, uint startTime_, uint duration_) {
         token = token_;
+        startTime = startTime_;
+        duration = duration_;
         for (uint i = 0; i < recipients_.length; i++) {
             allocation[recipients_[i]] = allocations_[i];
         }
--- test/LinearVesting.ts
+++ test/LinearVesting.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("LinearVesting", function () {
 
@@ -15,12 +15,15 @@ describe("LinearVesting", function () {
         const recipients = (await ethers.getSigners()).map(s => s.address)
         const allocations = recipients.map((r, idx) => ethers.utils.parseEther((idx * 10).toString()))
 
+        const startTime = (await time.latest()) + 60 // starts 60 seconds after deployment
+        const duration = 60 * 60 // 1 hour duration
+
         // deploy the contract
         const Contract = await ethers.getContractFactory("LinearVesting")
-        const contract = await Contract.deploy(token.address, recipients, allocations) // add the args
+        const contract = await Contract.deploy(token.address, recipients, allocations, startTime, duration) // add the args
         await contract.deployed()
 
-        return { contract, token, recipients, allocations }
+        return { contract, token, recipients, allocations, startTime, duration }
     }
 
     // test cases go here
@@ -36,6 +39,14 @@ describe("LinearVesting", function () {
                 const allocation = allocations[index];
                 expect(await contract.allocation(recipient)).to.eq(allocation)
             }
+        })
+        it("should have a start time", async function () {
+            const { contract, startTime } = await loadFixture(deploy)
+            expect(await contract.startTime()).to.eq(startTime)
+        })
+        it("should have a duration", async function () {
+            const { contract, duration } = await loadFixture(deploy)
+            expect(await contract.duration()).to.eq(duration)
         })
     })
 
